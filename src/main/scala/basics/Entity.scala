bc87efdcbb40d9b0d5d13d8d6c0c5bc387a12a43
Joseph Baker <jbaker1422@gmail.com>
2019-01-22 10:38:31 -0600

    I added an Entity with intersection

---
+++ src/main/scala/basics/Entity.scala
@@ -0,0 +1,12 @@
+package basics
+
+class Entity {
+  private var x = 0.0
+  private var y = 0.0
+  private var width = 1.0
+  private var height = 1.0
+  
+  def intersects ( a: Entity): Boolean = {
+    ???
+  }
+}
